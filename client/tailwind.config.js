d3f515687abddd482b17b1badf6c35d64b6ff6ce
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-10-08 15:33:50 +0100

    feat: files and projects assets upload

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1D2026",
+        primary_orange: "#FF6636",
+        primary_purple: "#564FFD",
+        success: "#23BD33",
+        warning: "#FD8E1F",
+        error: "#E34444",
+        light_gray: "#F5F7FA",
+      },
+      fontSize: {
+        heading: "72px",
+        sub_heading: "40px",
+        hero_text: "24px",
+        small_text: "14px",
+      },
+    },
   },
   plugins: [],
-}
+};
